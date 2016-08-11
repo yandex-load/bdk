0198f7584cca69c2ed156096ecde418cbbdcfc55
Alexey Lavrenuke <direvius@gmail.com>
2016-08-11 15:52:39 +0300

    entrypoint

--- setup.py
+++ setup.py
@@ -2,14 +2,20 @@
 
 from setuptools import setup
 
-setup(name='bdk',
-      version='0.0.1',
-      description='Tank manager',
-      author='Alexey Lavrenuke',
-      author_email='direvius@gmail.com',
-      url='https://github.yandex-team.ru/load/bdk/',
-      packages=['bdk'],
-      package_data={'bdk': []},
-      install_requires=[
-          'requests',
-      ], )
+setup(
+    name='bdk',
+    version='0.0.1',
+    description='Tank manager',
+    author='Alexey Lavrenuke',
+    author_email='direvius@gmail.com',
+    url='https://github.yandex-team.ru/load/bdk/',
+    packages=['bdk'],
+    package_data={'bdk': []},
+    install_requires=[
+        'requests',
+    ],
+    entry_points={
+        'console_scripts': [
+            'bdk = bdk.service:main',
+        ],
+    },)
