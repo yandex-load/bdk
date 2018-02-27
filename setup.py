831afe1151574fd47c0a125a6bfdb9f79dec4fb0
netort <netort@yandex-team.ru>
2018-02-27 21:08:48 +0300

    ver bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bdk',
-    version='0.0.6',
+    version='0.0.7',
     description='Task manager',
     author='Yandex-load team',
     author_email='load@yandex-team.ru',
