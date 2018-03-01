a6e9bdaf257ec3112b31fcea87a3f5623c633b92
netort <netort@yandex-team.ru>
2018-03-01 16:56:54 +0300

    ver bump, added config sample to py package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bdk',
-    version='0.0.7',
+    version='0.0.8',
     description='Task manager',
     author='Yandex-load team',
     author_email='load@yandex-team.ru',
@@ -23,5 +23,6 @@ setup(
     },
     package_data={
         'bdk.core': ['config/*'],
+        'bdk.core': ['config_sample/*'],
     },
 )
